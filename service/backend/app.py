725315c365aa86292abe5d679e96433f91ffea9e
Yuriy Nazarov <nazarov.yuriy.pavlovich@gmail.com>
2022-04-10 16:48:44 +0300

    Add backend on flask with dummy check engine

---
+++ service/backend/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+from flasgger import Swagger
+from libs.engines import EngineFactory
+
+app = Flask(__name__)
+swagger = Swagger(app)
+engine_factory = EngineFactory()
+
+
+@app.route("/api/v1/check", methods=["POST"])
+def check():
+    """
+    Check text
+    ---
+    parameters:
+      - name: engine
+        in: query
+        type: string
+        default: dummy
+      - name: body
+        in: body
+        schema:
+          properties:
+            text:
+              type: string
+          example:
+            {"text": "Don't need a consolidation agreement so we don't have to negotiate one."}
+    responses:
+      200:
+        description: Check results
+    """
+    engine = engine_factory.get_engine(request.args.get("engine"))
+    return jsonify(engine.check(request.json["text"]).serialize())
+
+
+app.run(debug=True)
---
+++ service/backend/libs/engines.py
@@ -0,0 +1,91 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Match:
+    def __init__(self, start: int, length: int, rule: str = None, quickfix: str = None, message: str = None):
+        self.start = start
+        self.length = length
+        self.rule = rule
+        self.quickfix = quickfix
+        self.message = message
+
+    def serialize(self):
+        return self.__dict__
+
+
+class CheckResult:
+    def __init__(self, version):
+        self.version = version
+        self.matches = []
+        self.fixed = None
+
+    def fill_fixed(self, text: str):
+        res = []
+        pos = 0
+        for match in self.matches:
+            if match.quickfix is None:
+                continue
+            assert pos <= match.start
+            res.append(text[pos:match.start])
+            res.append(match.quickfix)
+            pos = match.start + match.length
+        res.append(text[pos:])
+        self.fixed = "".join(res)
+
+    def serialize(self):
+        res = self.__dict__
+        res["matches"] = [x.serialize() for x in res["matches"]]
+        return res
+
+
+class BaseEngine(metaclass=ABCMeta):
+    @staticmethod
+    @abstractmethod
+    def name() -> str:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def version() -> str:
+        pass
+
+    @abstractmethod
+    def check(self, text: str) -> CheckResult:
+        pass
+
+
+class DummyEngine(BaseEngine):
+    @staticmethod
+    def name():
+        return "dummy"
+
+    @staticmethod
+    def version():
+        return "dummy 0.1"
+
+    def check(self, text: str) -> CheckResult:
+        res = CheckResult(self.version())
+        if "Don't need a consolidation agreement so we don't have to negotiate one." == text:
+            res.matches.append(Match(
+                36, 0, quickfix=","
+            ))
+        res.fill_fixed(text)
+        return res
+
+
+class EngineFactory:
+    def __init__(self):
+        self.engines = {
+            engine.name(): engine for engine in [
+                DummyEngine,
+            ]
+        }
+        self.cache = {}
+
+    def get_engine(self, name: str) -> BaseEngine:
+        if name not in self.cache:
+            if name in self.engines:
+                self.cache[name] = self.engines[name]()
+            else:
+                ...
+        return self.cache[name]
