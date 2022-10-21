018387a6145bf0f39c88d4d7204552e1cc9140a7
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-21 23:16:05 +0000

    Adding tarfile member sanitization to extractall()

--- service/backend/test/test_comma_engine.py
+++ service/backend/test/test_comma_engine.py
@@ -21,13 +21,51 @@ class TestCommaEngine(unittest.TestCase):
         if not os.path.exists(engine.model_path):
             filename = wget.download("https://storage.yandexcloud.net/jbos/comma-roberta-base-3domains-more-data.tar")
             with tarfile.open(filename) as tar:
-                tar.extractall('.')
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, ".")
 
         engine = CommaDistilRoberta()
         if not os.path.exists(engine.model_path):
             filename = wget.download("https://storage.yandexcloud.net/jbos/comma-distilroberta-base-3domains.tar")
             with tarfile.open(filename) as tar:
-                tar.extractall('.')
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, ".")
 
     def test_noop_roberta(self):
         engine = CommaRoberta()
